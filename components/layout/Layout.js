48361a4435b97154c6409a9ecd95063da2ff40ec
Rashmy Patwari <rashmy.patwari@ascension-external.org>
2021-12-05 09:55:34 -0500

    fixed cards

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,12 +1,10 @@
-import classNames from "classnames";
-import classes from '../home/home.module.css';
 import NavBar from "./navbar";
 
 export default function Layout(props){
     return(
         <div>
             <NavBar />
-            <main className={classes.main}>{props.children}</main>
+            <div>{props.children}</div>
         </div>
     )
 }
